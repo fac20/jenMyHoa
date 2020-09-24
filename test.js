67ae2e30bb2abf33df5b2a0721ef5046f2beed52
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-09-24 11:35:57 +0100

    dirty code 4

--- test.js
+++ test.js
@@ -1,14 +1,4 @@
 const hi = () => {
   return hello;
-
-  ("hello");
 };
-console.logs("hi";)
-;
-console.logs("hi";)
-;
-console.logs("bo";)
-;
-console.logs("bo";);
-console.logs("bo";);
-console.logs("bo";);
+console.log("hi");
